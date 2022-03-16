00760387e780c57d5403bae8d98e85890ebae9c7
SilvanRenggli <sili.renggli@gmail.com>
2022-03-16 21:28:24 +0100

    apply potion to hero

--- GodotProject/Adventures/Encounters/Goblin.gd
+++ GodotProject/Adventures/Encounters/Goblin.gd
@@ -10,10 +10,10 @@ var outcome
 var summary = ""
 
 func encounter(hero):
-	if (hero.strength >= 2):
+	if (hero.stats["strength"] >= 2):
 		summary += "I defeated a goblin."
 		outcome = OUTCOME.success
-	elif (hero.health < 2):
+	elif (hero.stats["health"] < 2):
 		summary += "Died fighting a goblin."
 		outcome = OUTCOME.death
 	else:
